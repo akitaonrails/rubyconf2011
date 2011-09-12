3e9b406a2da9736b299b4048f01fcf6ae6da39cb
AkitaOnRails <fabioakita@gmail.com>
2011-09-12 09:52:06 -0300

    Adding support for simple News listing.

--- lib/default.rb
+++ lib/default.rb
@@ -5,44 +5,40 @@ require "bundler/setup"
 Bundler.require(:default)
 
 include Nanoc3::Helpers::Blogging
-include Nanoc3::Helpers::Breadcrumbs
 include Nanoc3::Helpers::Capturing
 include Nanoc3::Helpers::Filtering
 include Nanoc3::Helpers::HTMLEscape
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
-include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Text
 include Nanoc3::Helpers::XMLSitemap
 
 def load_translations
   @translations = {}
-  I18n.available_locales.each do |locale|
-    @translations.merge!( locale => Psych.load_file("lib/#{locale}.yml") )
-  end
+  I18n.available_locales.each { |locale| @translations.merge!( locale => Psych.load_file("lib/#{locale}.yml") ) }
 end
 
 def t(key)
   load_translations unless @translations
-  result = @translations[@item[:locale]]
-  key.split(/\./).each do |subkey|
-    result = result[subkey]
-  end
-  result
+  key.split(/\./).inject(@translations[@item[:locale]]) { |result, subkey| result = result[subkey] }
 end
 
 def active_language(locale)
-  if @item[:locale] == locale
-    " class=\"activeLenguage\""
-  else
-    ""
-  end
+  @item[:locale] == locale ? " class=\"activeLenguage\"" : ""
 end
 
 def menu(item)
-  if @item[:menu] == item
-    " class=\"activeMenu\""
+  @item[:menu] == item ? " class=\"activeMenu\"" : ""
+end
+
+def parse_date(string, part)
+  if part == :month
+    t("whatsup.months")[string.split("/").first.to_i - 1]
   else
-    ""
+    string.split("/")[1]
   end
+end
+
+def recent_posts
+  @site.sorted_articles[0, 10].uniq { |article| article[:filename] }.select { |article| article[:language].to_sym == @item[:locale] }
 end
