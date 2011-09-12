93ef4a5ee38b650a28cf5f8e62d9d0fb9497cd51
AkitaOnRails <fabioakita@gmail.com>
2011-09-12 05:02:53 -0300

    Adding initial i18n support

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "http://rubygems.org"
 
 gem "nanoc3", "~> 3.2"
+gem 'nanoc-filesystem-i18n', :require => 'nanoc3/data_sources/filesystem_i18n'
+gem 'psych'
--- lib/default.rb
+++ lib/default.rb
@@ -1,8 +1,35 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default)
+
+def load_translations
+  @translations = {}
+  I18n.available_locales.each do |locale|
+    @translations.merge!( locale => Psych.load_file("lib/#{locale}.yml") )
+  end
+end
+
+def t(key)
+  load_translations unless @translations
+  result = @translations[@item[:locale]]
+  key.split(/\./).each do |subkey|
+    result = result[subkey]
+  end
+  result
+end
+
+def active_language(locale)
+  if @item[:locale] == locale
+    " class=\"activeLenguage\""
+  else
+    ""
+  end
+end
 
 def menu(item)
-  if @item[:menu] && @item[:menu] == item
+  if @item[:menu] == item
     " class=\"activeMenu\""
   else
     ""
