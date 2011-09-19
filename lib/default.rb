98c28f7aa5ebf32db752dcebe77e88e557765298
AkitaOnRails <fabioakita@gmail.com>
2011-09-18 22:19:50 -0300

    Fixing the social network buttons' URL

--- lib/default.rb
+++ lib/default.rb
@@ -13,6 +13,18 @@ include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Text
 include Nanoc3::Helpers::XMLSitemap
 
+def normalize_identifier(item)
+   if item.children.size > 0 || item.identifier == '/' || item.identifier == "/#{item[:locale]}/"
+    item.identifier + 'index.html'
+  else
+    page = item.identifier.gsub(/\/$/, '')
+    page = "/index" if page =~ /locale/
+    page = "/sitemap" if page =~ /sitemap/
+    page = "/robots" if page =~ /robots/
+    "#{page}.#{item[:extension]}"
+  end
+end
+
 def load_translations
   @translations = {}
   I18n.available_locales.each { |locale| @translations.merge!( locale => Psych.load_file("lib/#{locale}.yml") ) }
